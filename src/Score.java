52f84a3f6fd28dcaec002910f548337ff715ccab
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-05-14 18:01:01 +0100

    return_five_when_only_five_knocks_are_thrown

--- src/Score.java
+++ src/Score.java
@@ -1,8 +1,12 @@
 public class Score {
+
+    private String line;
+
     public Score(String line) {
+        this.line = line;
     }
 
     public int total() {
-        return 0;
+        return !line.equals("--------------------") ? 5 : 0;
     }
 }
--- tests/AcceptedScore.java
+++ tests/AcceptedScore.java
@@ -6,7 +6,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AcceptedScore {
 
     @Test
-    public void should_return_zero_when_any_knock_is_thrown() throws Exception {
+    public void should_return_zero_when_no_knocks_are_thrown() throws Exception {
         assertThat(new Score("--------------------").total(), is(0));
+    }
+
+    @Test
+    public void should_return_five_when_only_five_knocks_are_thrown() throws Exception {
+        assertThat(new Score("-5------------------").total(), is(5));
     }
 }
